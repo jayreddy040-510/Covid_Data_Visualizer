69a0c035c2bc395505855cd846280e5bff1f7af5
Jay Reddy <jayreddy@gmail.com>
2022-08-15 13:33:49 -0700

    Parses promise from cdc to return array - choropleth map working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
 
 const stateBoundaryURL = 'https://raw.githubusercontent.com/loganpowell/census-geojson/master/GeoJSON/20m/2021/state.json'
 const svg = d3.select('#canvas')
@@ -9,23 +9,17 @@ let pctPopVax;
 const getVaxData = async () => {
     try{
         let response = await fetch(`https://data.cdc.gov/resource/unsk-b7fc.json?$query=SELECT location, series_complete_pop_pct WHERE location NOT IN ('BP2','DD2','GU','AS','FM','IH2','MH','MP','PR','PW','VA2','VI','UM') ORDER BY date DESC, series_complete_pop_pct DESC LIMIT 52`);
-        return response.json();
+        let z = response.json();
+        return z;
     }catch(err){
         console.error(err);
     }
 }
-let banana = async () => {
-const apple = await getVaxData()
-    let pct = apple.find(state => state['location'] === 'LA').series_complete_pop_pct
-    console.log(pct);
-}
-banana();
 
-// typeof getVaxData().then(res => console.log(res.find(state => {
-//     return state['location'] === 'CA';
-// })['series_complete_pop_pct']));
+let data =  await getVaxData();
 
-
+const range = data[0].series_complete_pop_pct - data[51].series_complete_pop_pct
+const quintile = range/5;
 
 
 let drawMap = () => {
@@ -34,22 +28,25 @@ let drawMap = () => {
     .data(stateBoundaries)
     .enter().append('path')
     .attr('d', d3.geoPath().projection(projection))
+    .attr('stroke-width', '4.0')
     .attr('class','state')
     .attr('fill', (stateObj) => {
+        
         let name = stateObj['properties'].STUSPS
-        if (name <= 'L') {
-            return 'tomato'
-        } else {
-            return 'darkslateblue'
-        }
-        // return getVaxData().then( res => {
-        //     let pct;
-        //     let state = res.find(state => state['location'] === 'LA')
-        //     pct = state.series_complete_pop_pct
-        //     if (pct < 70) {
-        //         return 'red'
-        //     }
-        // })
+        // if (name <= 'M') {
+        //     return 'tomato'
+        // } else {
+        //     return 'darkslateblue'
+        // }
+        
+        let state = data.find(state => state['location'] === name)
+        let pct = state['series_complete_pop_pct']
+        if (pct < 80) return 'firebrick'
+            // pct = state.series_complete_pop_pct
+            // if (pct < 70) {
+            //     return('red')
+            // }
+        })
     //     const res = await getVaxData();
     //     let state = res.find(state => state['location'] === 'LA')
     //     pct = state.series_complete_pop_pct
@@ -58,16 +55,12 @@ let drawMap = () => {
     //     }
     //     return abc();
     // }
-    })
+    
+    .attr("data-tooltip", (d) => d['properties'].STUSPS)
 }
 
-// let orange = () => {
-// let bananana = () => {
-// console.log('inside')
-// }
-// return bananana();
-// }
-// orange();
+
+
 
 
 d3.json(stateBoundaryURL).then(
@@ -79,10 +72,16 @@ d3.json(stateBoundaryURL).then(
             drawMap();
         }
     }
-)
-
-
-
-
-
+    )
+    
+    
+    // let orange = () => {
+    // let bananana = () => {
+    // console.log('inside')
+    // }
+    // return bananana();
+    // }
+    // orange();
+    
+    
 })
