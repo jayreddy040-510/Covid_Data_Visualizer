f89a1be0db68a81827015e10992b3b5d26c11f2f
Jay Reddy <jayreddy@gmail.com>
2022-08-15 06:37:08 -0700

    Establishes minimal interaction with hover effects and dummy choropleth coloring

--- src/index.js
+++ src/index.js
@@ -6,47 +6,74 @@ const projection = d3.geoAlbers().scale(1250).translate([500,300])
 let stateBoundaries;
 let pctPopVax;
 
-const getData = async () => {
+const getVaxData = async () => {
     try{
-        let response = await fetch(`https://data.cdc.gov/resource/unsk-b7fc.json?$query=SELECT location, series_complete_pop_pct WHERE location NOT IN ('BP2','DD2','GU','AS','FM','IH2','MH','MP','PR','PW','VA2','VI','UM') ORDER BY date DESC, location LIMIT 52`);
+        let response = await fetch(`https://data.cdc.gov/resource/unsk-b7fc.json?$query=SELECT location, series_complete_pop_pct WHERE location NOT IN ('BP2','DD2','GU','AS','FM','IH2','MH','MP','PR','PW','VA2','VI','UM') ORDER BY date DESC, series_complete_pop_pct DESC LIMIT 52`);
         return response.json();
     }catch(err){
         console.error(err);
     }
 }
+let banana = async () => {
+const apple = await getVaxData()
+    let pct = apple.find(state => state['location'] === 'LA').series_complete_pop_pct
+    console.log(pct);
+}
+banana();
 
-getData().then(res => console.log(res[0]['series_complete_pop_pct']))
+// typeof getVaxData().then(res => console.log(res.find(state => {
+//     return state['location'] === 'CA';
+// })['series_complete_pop_pct']));
 
-let y = getData().then(res => console.log(res.find(state => {
-    return state['location'] === 'CA';
-})['series_complete_pop_pct']));
 
-    console.log(y)
 
 
 let drawMap = () => {
-    console.log(stateBoundaries)
+    // console.log(stateBoundaries)
     svg.selectAll("path")
     .data(stateBoundaries)
     .enter().append('path')
     .attr('d', d3.geoPath().projection(projection))
     .attr('class','state')
-    .attr('fill', 'darkslateblue')
     .attr('fill', (stateObj) => {
-        let name = stateObj['STUSPS']
-        getData().then(res => res.find(state => {
-            return state['location'] === name;
-        }))
+        let name = stateObj['properties'].STUSPS
+        if (name <= 'L') {
+            return 'tomato'
+        } else {
+            return 'darkslateblue'
+        }
+        // return getVaxData().then( res => {
+        //     let pct;
+        //     let state = res.find(state => state['location'] === 'LA')
+        //     pct = state.series_complete_pop_pct
+        //     if (pct < 70) {
+        //         return 'red'
+        //     }
+        // })
+    //     const res = await getVaxData();
+    //     let state = res.find(state => state['location'] === 'LA')
+    //     pct = state.series_complete_pop_pct
+    //     if (pct < 70) { let abc = () => {
+    //         console.log('red')
+    //     }
+    //     return abc();
+    // }
     })
-
 }
 
+// let orange = () => {
+// let bananana = () => {
+// console.log('inside')
+// }
+// return bananana();
+// }
+// orange();
 
 
 d3.json(stateBoundaryURL).then(
     (data, error) => {
         if(error) {
-            console.log(log);
+            console.log(error);
         } else {
             stateBoundaries = data.features;
             drawMap();
